f8feb528f5017a3b561fc46d077a1c988a569789
Ömer Can Demirel <omrcndmrl041@hotmail.com>
2024-02-07 18:40:59 +0300

    Section 4 Ended

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,30 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
-builder.Services.AddCors();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// Bu iki middlewarein eklenme sırası önemlidir.
+app.UseAuthentication(); // Geçerli bir token var mı?
+app.UseAuthorization(); // Tokenin içeriği neleri mümkün kılıyor?
 
 app.MapControllers();
 
